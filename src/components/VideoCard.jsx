8562af585e1636930123e565eb3f5e77a7ef7b3e
Oguz <tiredcavalry@gmail.com>
2025-06-10 17:41:42 +0300

    VideoCard güncellendi

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,11 +1,11 @@
 function VideoCard({ title, text, video_url }) {
   return (
-    <div className="flex flex-col md:flex-row gap-6 items-start bg-white rounded-2xl shadow-md p-6 h-[100%]">
-      <div className="flex-grow basis-1/3">
+    <div className="flex flex-col md:flex-row gap-6 items-start bg-white rounded-2xl shadow-md p-6">
+      <div className="md:flex-1 w-full">
         <h3 className="text-2xl font-semibold mb-2">{title}</h3>
         <p className="text-gray-700">{text}</p>
       </div>
-      <div className="flex-grow basis-1/2 h-64 md:h-96 relative rounded-lg overflow-hidden">
+      <div className="w-full md:flex-1 aspect-video relative rounded-lg overflow-hidden">
         <iframe
           className="absolute top-0 left-0 w-full h-full"
           src={video_url}
